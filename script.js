3b53667b8787c2b5b9b7dae288f4ea6047b3b19e
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-13 11:45:22 +0200

    feat: add sound to click

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ let ham3 = document.querySelector("#ham3");
 let ham4 = document.querySelector("#ham4");
 let ham5 = document.querySelector("#ham5");
 let ham6 = document.querySelector("#ham6");
+let pole1 = document.querySelector("#pole1");
+let pole2 = document.querySelector("#pole2");
+let pole3 = document.querySelector("#pole3");
+let pole4 = document.querySelector("#pole4");
+let pole5 = document.querySelector("#pole5");
+let pole6 = document.querySelector("#pole6");
 
 function randomePole() {
   ham1.classList.remove("active");
@@ -12,7 +18,14 @@ function randomePole() {
   ham4.classList.remove("active");
   ham5.classList.remove("active");
   ham6.classList.remove("active");
-  let numPole = "ham" + (Math.floor(Math.random() * 6) + 1);
-  console.log(numPole);
+  let randomNum = Math.floor(Math.random() * 6) + 1;
+  let numPole = document.getElementById("ham" + randomNum);
   numPole.classList.add("active");
 }
+setInterval(randomePole, 1500);
+
+pole1.addEventListener("click", () => {
+  if (ham1.classList.contains("active")) {
+    audio.play();
+  }
+});
