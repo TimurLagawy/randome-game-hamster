18a2754fe78296d7983ead8bbac641ece94b27fa
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-13 11:08:07 +0200

    feat: add some logic

---
+++ script.js
@@ -0,0 +1,18 @@
+let ham1 = document.querySelector("#ham1");
+let ham2 = document.querySelector("#ham2");
+let ham3 = document.querySelector("#ham3");
+let ham4 = document.querySelector("#ham4");
+let ham5 = document.querySelector("#ham5");
+let ham6 = document.querySelector("#ham6");
+
+function randomePole() {
+  ham1.classList.remove("active");
+  ham2.classList.remove("active");
+  ham3.classList.remove("active");
+  ham4.classList.remove("active");
+  ham5.classList.remove("active");
+  ham6.classList.remove("active");
+  let numPole = "ham" + (Math.floor(Math.random() * 6) + 1);
+  console.log(numPole);
+  numPole.classList.add("active");
+}
